17127d4d794c384d88dea63baeae97fa69018688
MqKeYan <163413821+MqKeYan@users.noreply.github.com>
2026-01-01 17:33:54 +0800

    Update hadoop-env.sh

--- notes/home/user/hadoop-3.4.2/etc/hadoop/hadoop-env.sh
+++ notes/home/user/hadoop-3.4.2/etc/hadoop/hadoop-env.sh
@@ -9,4 +9,4 @@
 
 # The java implementation to use. By default, this environment
 # variable is REQUIRED on ALL platforms except OS X!
-export JAVA_HOME=/usr/java/jdk-11.0.29  #删除原本前面的“#”，注意/usr/java/jdk-11.0.29为实际的Jdk安装目录
+export JAVA_HOME=/usr/java/jdk-11.0.29  #删除原本前面的“#”，添加Jdk的安装目录，注意/usr/java/jdk-11.0.29为实际的Jdk安装目录
