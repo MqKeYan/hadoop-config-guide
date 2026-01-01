bbfe161771840bb97a02b205cef0ca7fd404d77e
MqKeYan <163413821+MqKeYan@users.noreply.github.com>
2026-01-01 17:21:59 +0800

    Create hadoop-env.sh

---
+++ notes/home/user/hadoop-3.4.2/etc/hadoop/hadoop-env.sh
@@ -0,0 +1,12 @@
+###
+# Generic settings for HADOOP
+###
+
+# Technically, the only required environment variable is JAVA_HOME.
+# All others are optional.  However, the defaults are probably not
+# preferred.  Many sites configure these options outside of Hadoop,
+# such as in /etc/profile.d
+
+# The java implementation to use. By default, this environment
+# variable is REQUIRED on ALL platforms except OS X!
+export JAVA_HOME=/usr/java/jdk-11.0.29  #删除原本前面的“#”，注意/usr/java/jdk-11.0.29为实际的Jdk安装目录
